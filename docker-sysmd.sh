62ecd7782d7de07566dc830e83329937fd65239c
Eric Caleb <calebme@mail.com>
2018-02-28 13:45:07 +0000

    fix function syntax

--- docker-sysmd.sh
+++ docker-sysmd.sh
@@ -5,31 +5,33 @@ file2="/etc/systemd/system/docker-sysmd-containers.service"
 
 function images {
   echo "Images"
- if [[ -e "$file" ]]; then
-   echo Configuration file exists. You all good.
- elif [ $? -eq 0 ]
- then
-   echo "Copying and Enabling service ..."
-   cp lib/docker-sysmd.timer /etc/systemd/system/docker-sysmd.timer
-   cp lib/docker-sysmd.service /etc/systemd/system/docker-sysmd.service
-   systemctl enable docker-sysmd.timer && systemctl start docker-sysmd.timer
-   echo "Timer Clears out all Images. Runs daily! :)"
- else
+  if [[ $? -eq 0  ]]; then
+    if [[ -e "$file"  ]]; then
+      echo Configuration file exists. You all good.
+    else
+     echo "Copying and Enabling service ..."
+     cp lib/docker-sysmd.timer /etc/systemd/system/docker-sysmd.timer
+     cp lib/docker-sysmd.service /etc/systemd/system/docker-sysmd.service
+     systemctl enable docker-sysmd.timer && systemctl start docker-sysmd.timer
+     echo "Timer Clears out all Images. Runs daily! :)"
+    fi
+  else
    echo "Service/Timer configuration failed :("
- fi
+  fi
 }
 
 function containers {
   echo "Containers"
- if [[ -e "$file2" ]]; then
-   echo Configuration file exists. You all good.
- elif [ $? -eq 0 ]
- then
-   echo "Copying and Enabling service ..."
-   cp lib/docker-sysmd-containers.timer /etc/systemd/system/docker-sysmd-containers.timer
-   cp lib/docker-sysmd-containers.service /etc/systemd/system/docker-sysmd-containers.service
-   systemctl enable docker-sysmd-containers.timer && systemctl start docker-sysmd-containers.timer
-  echo "Timer Clears out all Containers. Runs daily! :)"
+ if [[ $? -eq 0 ]]; then
+   if [[ -e "$file2" ]]; then
+     echo Configuration file exists. You all good.
+   else
+     echo "Copying and Enabling service ..."
+     cp lib/docker-sysmd-containers.timer /etc/systemd/system/docker-sysmd-containers.timer
+     cp lib/docker-sysmd-containers.service /etc/systemd/system/docker-sysmd-containers.service
+     systemctl enable docker-sysmd-containers.timer && systemctl start docker-sysmd-containers.timer
+     echo "Timer Clears out all Containers. Runs daily! :)"
+   fi
  else
    echo "Service/Timer configuration failed :("
  fi
